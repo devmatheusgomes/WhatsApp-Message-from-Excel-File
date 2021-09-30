9829162dff0cba5d967df183edbd4c1e7eb9f5e8
devmatheusgomes <matheusg@me.com>
2021-09-30 12:45:59 -0300

    Random numbers were inserted in the waiting time between one conversation and another

--- WhatsAppSend.py
+++ WhatsAppSend.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 import easygui as ed
 import pandas as pd
+import random
 
 #Selecionando Planilha de Clientes
 planilha = ed.fileopenbox()
@@ -25,7 +26,7 @@ for i, razaos in enumerate(contatos_df['Razao']):
     link = f"https://web.whatsapp.com/send?phone=+55{numero}&text={texto}"
     navegador.get(link)
     while len(navegador.find_elements_by_id("side")) < 1:
-        time.sleep(15)
+        time.sleep(random.randrange(10, 20))
     navegador.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div/div/div[2]/div[1]/div/div[2]').send_keys(Keys.ENTER)
     time.sleep(15)
 ed.msgbox(msg="PROCESSO FINALIZADO")
