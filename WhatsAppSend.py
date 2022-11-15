f280b1c9a664c1d0255156ebd70e34d0a999ef77
Matheus Gomes <77637518+devmatheusgomes@users.noreply.github.com>
2022-11-14 23:45:50 -0300

    Update Application
    Update applied, and correction of APIs used in the development of the project, such as readaptation.

--- WhatsAppSend.py
+++ WhatsAppSend.py
@@ -1,5 +1,6 @@
 import urllib.parse
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import time
@@ -7,35 +8,43 @@ import easygui as ed
 import pandas as pd
 import random
 
+
 # Selecionando Planilha de Clientes
 planilha = ed.fileopenbox()
 contatos_df = pd.read_excel(planilha)
 
 #abrindo navegador e site pelo profile principal do usuario
-options = webdriver.ChromeOptions()
-options.add_argument(r"--user-data-dir=C:\Users\mathe\AppData\Local\Google\Chrome\User Data")
-navegador = webdriver.Chrome(executable_path=r'C:\Path\chromedriver.exe', chrome_options=options)
->>>>>>> master
+Options = webdriver.ChromeOptions()
+#Altere aqui o caminho para o seu profile do chrome
+Options.add_argument(r"--user-data-dir=c:\users\Matheus Gomes\AppData\Local\Google\Chrome\User Data")
+#Altere aqui para o caminho do seu chromedriver
+navegador = webdriver.Chrome(executable_path=r'c:\WebDriver\chromedriver.exe', chrome_options=Options)
+
 navegador.get("https://web.whatsapp.com/")
 
-# esperando abrir conversas
-while len(navegador.find_elements_by_id("side")) < 1:
-    time.sleep(10)
+#Esperando abrir janela de conversas do whatsapp
+navegador.find_elements(By.ID, "side")
+time.sleep(10)
 
-# lendo planilha e enviando menasgns
+#Lendo planilha e enviando mensagens
 for i, razaos in enumerate(contatos_df['Razao']):
     razaosocial = contatos_df.loc[i, "Razao"]
     numero = contatos_df.loc[i, "Numero"]
     texto = urllib.parse.quote(f"Bom dia, a respeito da empresa {razaosocial}, ")
     link = f"https://web.whatsapp.com/send?phone=+55{numero}&text={texto}"
     navegador.get(link)
-    while len(navegador.find_elements_by_id("side")) < 1:
-        time.sleep(random.randrange(10, 15))
-    try:
-        navegador.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div/div/div[2]/div[1]/div/div[2]').send_keys(Keys.ENTER)
-        time.sleep(random.randrange(5, 13))
-    except Exception:
-        pass
+    while len(navegador.find_elements(By.ID, "side")) < 1:
+        time.sleep(random.randrange(5, 8))
 
-ed.msgbox(msg="PROCESSO FINALIZADO", ok_button='OK')
+        #Validando Numero do WhatsApp, se for verdadeira, envia mensagem
+        if len(navegador.find_elements(By.XPATH, '//*[@id="app"]/div/span[2]/div/span/div/div/div/div/div/div[1]')) < 1:
+            #Enviando mensagem com numero valido
+            navegador.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button/span').click()
+            time.sleep(2)
+            #Avisando o Usuario do Numero Invalido com os dados
+        else: ed.msgbox(msg=f"O Número {numero} da Empresa {razaosocial} é Inválido", ok_button="Continuar")
+
+#fechando navegador aberto
+navegador.quit()
+ed.msgbox(msg="PROCESSO FINALIZADO", ok_button='OK', title='FIM')
 
